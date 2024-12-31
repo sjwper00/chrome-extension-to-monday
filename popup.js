944131a8bd2438c9fabf8fdfcedea97c86563f0c
sjwper00 <40692150+sjwper00@users.noreply.github.com>
2024-12-31 22:10:00 +0900

    Update popup.js
    change the code from popup.html to popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Web to Monday.com</title>
-</head>
-<body>
-  <h1>Sync to Monday.com</h1>
-  <button id="sync">Start Sync</button>
-  <script src="popup.js"></script>
-</body>
-</html>
+document.getElementById("sync").addEventListener("click", () => {
+  chrome.runtime.sendMessage({ type: "start_sync" });
+});
+
